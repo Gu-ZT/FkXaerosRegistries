33c30780c2bdc5ea7dfa3c96bf4b57c61275c384
Gu-ZT <administration@ztxy666.cn>
2026-08-21 13:41:19 +0800

    init: 初始化项目

---
+++ src/main/java/dev/dubhe/gugle/xaero/FkXaerosRegistries.java
@@ -0,0 +1,10 @@
+package dev.dubhe.gugle.xaero;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FkXaerosRegistries implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
